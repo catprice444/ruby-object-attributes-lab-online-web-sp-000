bc50d3175995519056672506fa07299027ca3344
Catherine Price <catprice444@gmail.com>
2020-05-12 00:36:03 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,15 +1,15 @@
 class Dog 
-  def name(dogs_name)
+  def name=(dogs_name)
     @name = dogs_name
   end
   
-  def name=(name)
+  def name
     @name
   end 
-    def breed(dogs_breed)
+    def breed=(dogs_breed)
       @breed = dogs_breed
     end
-    def breed=(breed)
+    def breed
       @breed
     end
 end 
